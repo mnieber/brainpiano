ffa4dfde77867afb69da0f45b58f696a08ac6027
Maarten Nieber <hallomaarten@yahoo.com>
2021-05-05 00:51:31 +0200

    Weigh groups by size when selecting a random chord

--- frontend/src/quiz/utils/createQuery.ts
+++ frontend/src/quiz/utils/createQuery.ts
@@ -1,4 +1,4 @@
-import { range } from 'lodash/fp';
+import { range, sumBy } from 'lodash/fp';
 import { randomElement } from 'src/utils/random';
 import { ClefT } from 'src/keyboard/types';
 import { GroupT } from 'src/groups/types';
@@ -11,18 +11,39 @@ const getRandomInversion = (voicing: VoicingT, clef: ClefT) => {
   const [minInversion, maxInversion] = getInversionRange(
     voicingToChord(voicing, clef, 1)
   );
-  return randomElement(
-    range(
-      Math.max(0, minInversion),
-      Math.min(voicing.chord.length, maxInversion + 1)
-    )
-  );
+  let inversion = randomElement(range(minInversion, maxInversion + 1));
+
+  // Try to bring inversion closer to zero using steps of voicing.chord.length inversions
+  // (which means its the same inversion, just at a different place on the keyboard)
+  while (
+    inversion + voicing.chord.length <= maxInversion &&
+    Math.abs(inversion) > Math.abs(inversion + voicing.chord.length)
+  ) {
+    inversion += voicing.chord.length;
+  }
+  while (
+    inversion - voicing.chord.length >= minInversion &&
+    Math.abs(inversion) > Math.abs(inversion - voicing.chord.length)
+  ) {
+    inversion -= voicing.chord.length;
+  }
+  return inversion;
 };
 
 export const createQuery = (clefs: ClefT[], groups: GroupT[]): QueryT => {
   const clef = randomElement(clefs);
-  const group = randomElement(groups);
-  const voicing = randomElement(group.voicings);
+  const nrVoicings = sumBy((x: GroupT) => x.voicings.length)(groups);
+  const voicingIdx = Math.floor(Math.random() * nrVoicings);
+
+  let groupIdx = 0;
+  let count = groups[0].voicings.length;
+  while (voicingIdx >= count) {
+    groupIdx += 1;
+    count += groups[groupIdx].voicings.length;
+  }
+
+  const group = groups[groupIdx];
+  const voicing = group.voicings[voicingIdx - (count - group.voicings.length)];
   const inversion = getRandomInversion(voicing, clef);
 
   return {

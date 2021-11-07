541f9743d81acc9747fd923744986b6424a110a8
Maarten Nieber <hallomaarten@yahoo.com>
2021-11-07 19:27:34 +0100

    Arpeggiate chord

--- frontend/src/voicings/utils/playChord.ts
+++ frontend/src/voicings/utils/playChord.ts
@@ -1,8 +1,7 @@
-import { ChordT } from 'src/voicings/types';
-import { ClefT } from 'src/keyboard/types';
 import AudioSynth from 'audiosynth';
-import { forEach } from 'lodash/fp';
 import { clefOffsets } from 'src/keyboard/keyConstants';
+import { ClefT } from 'src/keyboard/types';
+import { ChordT } from 'src/voicings/types';
 
 var AudioContext = window.AudioContext || (window as any).webkitAudioContext;
 
@@ -10,6 +9,10 @@ var context = new AudioContext();
 var synth = new AudioSynth(context);
 
 export const playChord = (chord: ChordT, clef: ClefT) => {
-  forEach((note) => synth.playNote(note + 48, 1.0, 1.0, 0), chord);
+  chord.forEach((note, idx) =>
+    window.setTimeout(() => {
+      synth.playNote(note + 48, 0.5, 0.75, 0);
+    }, 200 + idx * 150)
+  );
   synth.playNote(clefOffsets[clef] + 36, 1.0, 1.0, 0);
 };

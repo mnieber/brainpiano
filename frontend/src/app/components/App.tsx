f2349ae2be757bfd82e1c70647529258ace02b89
Maarten Nieber <hallomaarten@yahoo.com>
2021-11-10 11:55:13 +0100

    Tell user to use landscape rotation

--- frontend/src/app/components/App.tsx
+++ frontend/src/app/components/App.tsx
@@ -1,10 +1,31 @@
+import { observer } from 'mobx-react-lite';
+import React from 'react';
 import { UrlRouter } from 'src/app/components/UrlRouter';
+import { useStore } from 'src/app/components/useStore';
 import './App.scss';
 
-export function App() {
-  return (
-    <div className="App w-screen">
-      <UrlRouter />
+export const App = observer(() => {
+  const appStore = useStore();
+
+  React.useEffect(() => {
+    function handleResize() {
+      appStore.setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+  });
+
+  const isPortrait = appStore.scaleFactor < 0.4;
+  const useLandscapeDiv = (
+    <div className="App__useLandscape">
+      <div className="text-2xl my-auto mx-4">
+        Please rotate your device to landscape mode
+      </div>
     </div>
   );
-}
+  return (
+    <div className="App w-screen">
+      {isPortrait ? useLandscapeDiv : <UrlRouter />}
+    </div>
+  );
+});
--- frontend/src/quiz/components/QuizFrame.tsx
+++ frontend/src/quiz/components/QuizFrame.tsx
@@ -1,23 +1,12 @@
 import classnames from 'classnames';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
-import { useStore } from 'src/app/components';
 import { QuizFrameTopPanel } from 'src/quiz/components';
 import './QuizFrame.scss';
 
 type PropsT = React.PropsWithChildren<{}>;
 
 export const QuizFrame: React.FC<PropsT> = observer((props: PropsT) => {
-  const appStore = useStore();
-
-  React.useEffect(() => {
-    function handleResize() {
-      appStore.setWindowWidth(window.innerWidth);
-    }
-
-    window.addEventListener('resize', handleResize);
-  });
-
   return (
     <div className={classnames('QuizFrame flex flex-col px-2')}>
       <QuizFrameTopPanel />
